98efc3b5f9a11828d3f0b8b121111b069761e91d
Nicolas GOASGUEN <n.goasguen@gmail.com>
2025-03-06 22:12:15 +0100

    Fix database types and domains

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,17 @@
-CREATE DOMAIN email AS TEXT CHECK (VALUE ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE DOMAIN email_address AS citext CHECK (VALUE ~ '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+[.][A-Za-z]{2,}$');
+CREATE DOMAIN code_4_digits AS TEXT CHECK (LENGTH(VALUE) = 4 AND VALUE ~ '^[0-9]{4}$');
+
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
-    email email UNIQUE,
+    email email_address UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     is_active BOOLEAN DEFAULT FALSE
 );
 CREATE TABLE IF NOT EXISTS verification_codes (
     id SERIAL PRIMARY KEY,
-    email email REFERENCES users(email) ON DELETE CASCADE,
-    code TEXT NOT NULL CHECK (LENGTH(code) = 4 AND code ~ '^[0-9]{4}$'),
+    email email_address REFERENCES users(email) ON DELETE CASCADE,
+    code code_4_digits NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
